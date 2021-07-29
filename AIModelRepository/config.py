146d51594deef3b35912d94b4d5833d004006fda
jaewonMoon <jaewonmoon.keti@gmail.com>
2021-07-29 09:07:17 +0900

    respository

---
+++ AIModelRepository/config.py
@@ -0,0 +1,13 @@
+'''
+Created on Apr 26, 2019
+
+@author: moonjaewon
+'''
+
+DEBUG= True
+TESTING = False
+MODEL_DIRECTORY ='model'
+BASE_URL = 'localhost'
+PORT ='8009'
+FULL_BASE_URL ='http://'+BASE_URL
+DOWNLOAD_FOLDER='download/model/'
